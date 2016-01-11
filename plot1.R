9d8388f5b98e4d37d931d0c78d42b7e2d06e0214
John Price <jphilip24@gmail.com>
2016-01-11 17:52:16 -0500

    Added plot 1 code and image.

---
+++ plot1.R
@@ -0,0 +1,30 @@
+# Create Plot 1
+
+# Load in the original data and subset to the required days.
+# The original datafile was not pushed to Github due to its size.
+data1 <- read.csv2("household_power_consumption.txt", stringsAsFactors = FALSE)
+data2 <- subset(data1, data1$Date == "1/2/2007" | data1$Date == "2/2/2007")
+
+# Create a datetime value from the separate Date and Time character values
+data2$Datetime <- strptime(paste(data2$Date, data2$Time), "%d/%m/%Y %H:%M:%S")
+
+# Convert character values to numeric
+data2$Global_active_power <- as.numeric(data2$Global_active_power)
+data2$Global_reactive_power <- as.numeric(data2$Global_reactive_power)
+data2$Voltage <- as.numeric(data2$Voltage)
+data2$Sub_metering_1 <- as.numeric(data2$Sub_metering_1)
+data2$Sub_metering_2 <- as.numeric(data2$Sub_metering_2)
+data2$Sub_metering_3 <- as.numeric(data2$Sub_metering_3)
+
+# Open PNG file and set single plot format
+png("plot1.png")
+par(mfrow = c(1,1))
+
+# Plot 1
+hist(data2$Global_active_power,
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+# Clos PNG file
+dev.off()
